c298b422b475c887cf31d365e19498212d7b51aa
ericsei <nawnaw88@gmail.com>
2019-03-08 13:19:10 -0500

    Updated

--- productsearch/settings.py
+++ productsearch/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -78,7 +77,7 @@ WSGI_APPLICATION = 'productsearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql__psycopg2',
         'NAME': 'productsearchdb',
         'USER':'postgres',
         'PASSWORD': '',
