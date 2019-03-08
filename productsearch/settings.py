7905f1d0f64ba92a5d1f274046740aeea6aa52e9
ericsei <nawnaw88@gmail.com>
2019-03-08 18:30:38 -0500

    Updated

--- productsearch/settings.py
+++ productsearch/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'productsearch/static')
+    os.path.join(BASE_DIR, 'productsearch/static/')
 ]
 
 
